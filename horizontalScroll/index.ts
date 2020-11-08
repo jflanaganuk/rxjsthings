81be6b9a54726aefeca27e964aa8444272b0c6f1
Joshua Flanagan <josh_flanagan@live.co.uk>
2020-11-08 13:28:56 +0000

    added scroll

---
+++ horizontalScroll/index.ts
@@ -0,0 +1,15 @@
+import { fromEvent } from "rxjs";
+import { tap, throttleTime } from "rxjs/operators";
+
+const scrollIndication = document.getElementById('indication');
+const getScrollWidth = () => {
+    const doc = document.documentElement;
+    const winScroll = doc.scrollTop;
+    const height = doc.scrollHeight - doc.clientHeight;
+    return (winScroll/height)*100;
+};
+const setScroll = () => (scrollIndication!.style.width = getScrollWidth() + '%');
+
+fromEvent(document, 'scroll')
+    .pipe(throttleTime(20), tap(setScroll))
+    .subscribe();
